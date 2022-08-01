eb59a55ff08d71548b6bf03d2c40f5ccc4d027d4
izzyjere <wisdomjere5@gmail.com>
2022-08-01 20:58:13 +0200

    IOS Implementations

---
+++ EHymns/Platforms/iOS/DeviceInfoImplementationiOS.cs
@@ -0,0 +1,25 @@
+﻿using EHymns.Platforms.iOS;
+using EHymns.Shared;
+
+using Foundation;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UIKit;
+
+
+
+[assembly: Dependency(typeof(DeviceInfoImplementationiOS))]
+namespace EHymns.Platforms.iOS
+{
+    public class DeviceInfoImplementationiOS : EHymns.Interfaces.IDeviceInfo
+    {
+        public bool IsWhatsAppInstalled()
+        {
+            return UIApplication.SharedApplication.CanOpenUrl(new NSUrl(new NSString("whatsapp://")));
+
+        }
+    }
+}
